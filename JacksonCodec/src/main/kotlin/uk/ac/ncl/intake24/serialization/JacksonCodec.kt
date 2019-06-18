3d5f522101df3600c7db29c019d72d5a66b98f74
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2019-06-18 14:35:45 +0100

    Proper encoding of dates

--- JacksonCodec/src/main/kotlin/uk/ac/ncl/intake24/serialization/JacksonCodec.kt
+++ JacksonCodec/src/main/kotlin/uk/ac/ncl/intake24/serialization/JacksonCodec.kt
@@ -1,12 +1,21 @@
 package uk.ac.ncl.intake24.serialization
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.google.inject.Singleton
 import kotlin.reflect.KClass
 
 @Singleton
 class JacksonCodec : StringCodec {
     private val mapper = ObjectMapper()
+
+    init {
+        mapper
+                .registerModule(JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+
+    }
 
     override fun <T> encode(obj: T): String {
         return mapper.writeValueAsString(obj)
