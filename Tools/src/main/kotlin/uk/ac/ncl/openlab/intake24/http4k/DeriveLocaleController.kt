3effc578c2b954f1bf4909d50e61e3355eafe9a5
Ivan Poliakov <ivpolyakov@gmail.com>
2019-07-15 15:23:56 +0100

    Temporary hard coded FCT id

--- Tools/src/main/kotlin/uk/ac/ncl/openlab/intake24/http4k/DeriveLocaleController.kt
+++ Tools/src/main/kotlin/uk/ac/ncl/openlab/intake24/http4k/DeriveLocaleController.kt
@@ -1,6 +1,5 @@
 package uk.ac.ncl.openlab.intake24.http4k
 
-import arrow.core.Either
 import com.google.inject.Inject
 import org.http4k.core.MultipartFormBody
 import org.http4k.core.Request
@@ -8,7 +7,6 @@ import org.http4k.core.Response
 import org.http4k.core.Status
 import org.slf4j.LoggerFactory
 import uk.ac.ncl.intake24.serialization.StringCodec
-import uk.ac.ncl.openlab.intake24.tools.CsvParseException
 import uk.ac.ncl.openlab.intake24.tools.DeriveLocaleCsvParser
 import uk.ac.ncl.openlab.intake24.tools.DeriveLocaleParseException
 import uk.ac.ncl.openlab.intake24.tools.DeriveLocaleService
@@ -26,10 +24,10 @@ class DeriveLocaleController @Inject constructor(private val service: DeriveLoca
         val form = MultipartFormBody.from(request)
         val file = form.file("file")
         val sourceLocale = form.field("sourceLocale")
-        val destLocale = form.field("destLocale")
-        val foodCompositionTable = form.field("foodCompositionTable")
+        val targetLocale = form.field("targetLocale")
+        val foodCompositionTable = "NDNS"
 
-        if (file == null || sourceLocale == null || destLocale == null || foodCompositionTable == null)
+        if (file == null || sourceLocale == null || targetLocale == null || foodCompositionTable == null)
             return errorUtils.errorResponse(Status.BAD_REQUEST, "Missing required field")
         else {
 
@@ -39,7 +37,7 @@ class DeriveLocaleController @Inject constructor(private val service: DeriveLoca
                 if (errors.isNotEmpty())
                     return Response(Status.BAD_REQUEST).body(stringCodec.encode(ErrorsResponse(errors)))
                 else {
-                    service.deriveLocale(sourceLocale, destLocale, actions)
+                    service.deriveLocale(sourceLocale, targetLocale, actions)
                     return Response(Status.OK)
                 }
             } catch (e: DeriveLocaleParseException) {
