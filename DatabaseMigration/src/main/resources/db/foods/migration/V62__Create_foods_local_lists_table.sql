ab234dc9f96b42099bd0bc3f3896c1cf92461c65
Ivan Poliakov <ivpolyakov@gmail.com>
2019-07-31 12:37:49 +0100

    Add missing update/delete statements to foreign key

--- DatabaseMigration/src/main/resources/db/foods/migration/V62__Create_foods_local_lists_table.sql
+++ DatabaseMigration/src/main/resources/db/foods/migration/V62__Create_foods_local_lists_table.sql
@@ -1,6 +1,6 @@
 create table foods_local_lists
 (
-    locale_id varchar(16) references locales (id),
-    food_code varchar(8) references foods (code),
+    locale_id varchar(16) references locales (id) on update cascade on delete cascade,
+    food_code varchar(8) references foods (code) on update cascade on delete cascade,
     constraint foods_locale_inclusion_pk primary key (locale_id, food_code)
 );
