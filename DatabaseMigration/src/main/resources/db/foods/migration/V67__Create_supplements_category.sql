e3eabda86e53e031db68ace88a77c7f1fd946b25
Ivan Poliakov <ivpolyakov@gmail.com>
2019-09-18 16:40:24 +0100

    Add SUPP category

---
+++ DatabaseMigration/src/main/resources/db/foods/migration/V67__Create_supplements_category.sql
@@ -0,0 +1,2 @@
+insert into categories values('SUPP', 'Dietary supplements', false, '2d9fe3cf-15bd-4aba-a5d2-f5ffe1d6a326');
+insert into categories_local values('SUPP', 'en_GB', 'Dietary supplements', 'Dietary supplements', 'c0e1b31a-dd59-4919-8098-ff82bfd1e56c');
